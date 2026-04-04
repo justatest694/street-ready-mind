259a1aa554aeea0405b16cb3038f0c196b704990
justatest694 <Davidsbbrandao@hotmail.com>
2026-04-04 14:16:29 +0100

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -5,7 +5,7 @@ import { componentTagger } from "lovable-tagger";
 
 // https://vitejs.dev/config/
 export default defineConfig(({ mode }) => ({
-  base: '/',
+  base: '/street-ready-mind/',
   server: {
     host: "::",
     port: 8080,
